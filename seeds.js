49bf5535e6e795acc1b9a402c671686e55da1f10
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-08-01 14:07:03 -0400

    comments association

--- seeds.js
+++ seeds.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var Campground = require('./models/campground');
+var Comment = require('./models/comment');
 
 var data = [
     {
@@ -32,11 +33,26 @@ function seedDB(){
         console.log('removed user data succesfully');
         //add few campgrounds//
         data.forEach(function(seed){
-            Campground.create(seed, function(err, data){
+            Campground.create(seed, function(err, campground){
                 if(err){
                     console.log(err);
                 }else{
                     console.log('seed.js successfull');
+                    //create a comment//
+                    Comment.create(
+                        {
+                            text: 'this place is great',
+                            author: 'pancholi'
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            }else{
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log('created comment');
+                            }
+                            
+                        });
                 }
             });
         });
