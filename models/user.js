41f0af24d721539dfba9f79f4e9a859d25f9f1d4
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-08-06 18:32:55 -0400

    sign up functionality troubleshoot

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-    passportLocalMongoose = require('passport-local-mongoose');
+    passportLocalMongoose = require('passport-local-mongoose')
 
 var userSchema = mongoose.Schema({
     username: String,
