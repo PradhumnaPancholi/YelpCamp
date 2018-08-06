281a0cc42846fe82c47a0d4092f8ab9e02d81e1d
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-08-06 17:36:00 -0400

    auth and user model setup troubleshoot

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-    passportLocalMongoose = requiq('passport-local-mongoose');
+    passportLocalMongoose = require('passport-local-mongoose');
 
 var userSchema = mongoose.Schema({
     username: String,
