6d462b4dec5510a5e33f37d91cee1444506441d8
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-08-07 17:20:31 -0400

    logput functionality and navbar improv

--- app.js
+++ app.js
@@ -149,5 +149,11 @@ app.post('/login', passport.authenticate('local',
         failureRedirect: '/login'
     }),function(req, res){
 });
+
+//Log out functionality//
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/campgrounds');
+});
 //==================================================================================================//
 app.listen(process.env.PORT, process.env.IP);
