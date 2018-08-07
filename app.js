c39f53c19b6dd82a67a832835fa560db3d71b26a
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-08-07 18:03:01 -0400

    added navbar functionality with req.user

--- app.js
+++ app.js
@@ -26,6 +26,10 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 
 //landing page//
