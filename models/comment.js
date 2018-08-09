15f9f575452ad4a1d0e31c1a2da30faf8a0d38a9
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-08-08 20:39:57 -0400

    user and campground data association 03

--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
  var commentSchema = mongoose.Schema({
      text: String,
-     author: {
-         id:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
+     author:{
+         id: {
+             type: mongoose.Schema.Types.ObjectId,
+             ref: 'User'
          },
-            username: String
+         username: String 
      }
  });
 module.exports = mongoose.model('Comment', commentSchema); 
